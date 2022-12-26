31b9878a3e5a4ebc588c3a0051a40c6ccc0c6b1f
Musa AbdulHameed Lawal <lekzy.csharp@gmail.com>
2022-12-26 21:11:40 +0100

    restructure models

--- models/service_providers.go
+++ models/service_providers.go
@@ -11,6 +11,19 @@ type ServiceProvider struct {
 	PhoneNumber             string
 	Email                   string
 	Password                string
+	IsFullyOnboarded        bool
+	IsAccountConfirmed      bool
+	Pin                     string
+}
+
+type ServiceProviderJson struct {
+	FirstName   string `json:"first_name" gorm:"firstName" validate:"required"`
+	LastName    string `json:"last_name" gorm:"lastName" validate:"required"`
+	PhoneNumber string `json:"phone_number" gorm:"phoneNumber" validate:"required"`
+	Email       string `json:"email" gorm:"email" validate:"required"`
+	Password    string `json:"password" gorm:"password" validate:"required"`
+	Pin         string `json:"pin" gorm:"pin" validate:"required"`
+	Address     ServiceProviderAddressJson
 }
 
 type ServiceProviderAddress struct {
@@ -20,4 +33,10 @@ type ServiceProviderAddress struct {
 	Name                           string
 	ZipCode                        string
 	City                           string
+}
+
+type ServiceProviderAddressJson struct {
+	Name    string `json:"name" gorm:"name" validate:"required"`
+	ZipCode string `json:"zip_code" gorm:"zipCode" validate:"required"`
+	City    string `json:"city" gorm:"city" validate:"required"`
 }
