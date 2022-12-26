9edf2e4d7d641302e7f457f312b988279fbe9fd9
Musa AbdulHameed Lawal <lekzy.csharp@gmail.com>
2022-12-26 20:41:49 +0100

    generate user id

---
+++ pkg/commons.go
@@ -0,0 +1,36 @@
+package pkg
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz1234567890"
+const stringLength = 10
+
+func GenerateUserId(userFirstTwoChar string) string {
+	rand.Seed(time.Now().UnixNano())
+
+	//strings.Builder{} is used to concatenate character and avoid copy
+	//and also optimize memo allocatio
+	sb := strings.Builder{}
+
+	//Grow grows the string builder
+	sb.Grow(stringLength)
+	for i := 0; i < stringLength; i++ {
+		sb.WriteByte(charset[rand.Intn(len(charset))])
+	}
+	return userFirstTwoChar + sb.String()
+}
+
+func GetUserFirstTwoChar(firstName string) string {
+	var result string
+	for index, character := range firstName {
+		if index == 3 {
+			break
+		}
+		result += string(character)
+	}
+	return strings.ToLower(result) + "-"
+}
