6fbf1d1d2caba9b2d1dbe660dc087b67ff13ca4d
Musa AbdulHameed Lawal <lekzy.csharp@gmail.com>
2022-12-26 16:13:20 +0100

    service services

---
+++ client/service_services.go
@@ -0,0 +1,84 @@
+package client
+
+import (
+	"getservices/models"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type ServiceServices interface {
+	Create(cl models.Services) error
+	Service(serviceId string) (models.Services, error)
+	Services() ([]models.Services, error)
+	ServiceByEmail(email string) (models.Services, error)
+	ServiceByPhoneNumber(phoneNumber string) (models.Services, error)
+	Update(serviceId string, cl models.Client) error
+	DeactivateAccount(serviceId string) error
+	ActivateAccount(email string) (models.Client, error)
+}
+
+type ServiceAccount struct {
+	db *gorm.DB
+}
+
+func NewServiceAccount(db *gorm.DB) ServiceAccount {
+	return ServiceAccount{
+		db: db,
+	}
+}
+
+func (service ServiceAccount) Create(serv models.Services) error {
+	serv.CreatedAt = time.Now()
+	serv.UpdatedAt = time.Now()
+
+	data := models.Services{
+		SericesIdentity:   serv.SericesIdentity, // i do not remeber what it does..
+		ServiceProviderId: serv.ServiceProviderId,
+		PhoneNumber:       serv.PhoneNumber,
+		YearOfExperience:  serv.YearOfExperience,
+		CompanyName:       serv.CompanyName,
+		Email:             serv.Email,
+	}
+	return service.db.Create(data).Error
+}
+
+func (service ServiceAccount) Service(serviceId string) (models.Services, error) {
+	var s models.Services
+	return s, service.db.Where("client_id = ?", serviceId).First(&s).Error
+}
+
+func (service ServiceAccount) Services() ([]models.Services, error) {
+	var c []models.Services
+	return c, service.db.Find(&c).Error
+}
+
+func (service ServiceAccount) ServiceByEmail(email string) (models.Services, error) {
+	var s models.Services
+	return s, service.db.Where("email = ?", email).First(&s).Error
+}
+
+func (service ServiceAccount) ClientByPhoneNumber(phoneNumber string) (models.Services, error) {
+	var s models.Services
+	return s, service.db.Where("phone_number = ?", phoneNumber).First(&s).Error
+}
+
+func (service ServiceAccount) Update(clientId string, data models.Services) error {
+	c := models.Services{
+		CompanyName:      data.CompanyName,
+		YearOfExperience: data.YearOfExperience,
+		Email:            data.Email,
+		PhoneNumber:      data.PhoneNumber,
+	}
+	return service.db.Where("client_id = ?", clientId).Updates(&c).Error
+}
+
+func (service ServiceAccount) DeactivateAccount(clientId string) error {
+	var s models.Client
+	return service.db.Model(&s).Where("client_id = ?", clientId).Delete(s).Error
+}
+
+// complete activate account later
+func (client ServiceAccount) ActivateAccount(clientId string) error {
+	return nil
+}
