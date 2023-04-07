725289bed5e6550bac0b359073c59248e00ba7a6
Musa AbdulHameed Lawal <lekzy.csharp@gmail.com>
2023-04-08 00:55:05 +0100

    more fixes for dep

--- handlers/clients.go
+++ handlers/clients.go
@@ -10,7 +10,7 @@ import (
 	"github.com/DecodeWorms/getservices/pkg"
 	"github.com/DecodeWorms/getservices/storage"
 	"github.com/DecodeWorms/getservices/validations"
-	validator "github.com/go-playground/validator/v10"
+	"github.com/go-playground/validator"
 
 	//"github.com/go-playground/validator"
 
